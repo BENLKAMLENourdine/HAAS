9dce15253492118d149da7f9e9a43b00793e2be2
BENLKAMLENourdine <benlkamle.tiznit.95@gmail.com>
2018-12-08 21:50:35 +0100

    finished first version of calculateHash*

--- server/handlers/hash.js
+++ server/handlers/hash.js
@@ -10,3 +10,24 @@ exports.generateDummyHash = async (req, res, next) => {
     });
   }
 };
+
+exports.calculateHash = async (req, res, next) => {
+    try {
+        const Crypto = require('crypto-js');
+        const data = req.body.data
+        const iteration = req.body.iteration
+        const algorithm = req.body.algorithm
+
+        const hash = await Crypto.MD5(data).toString()
+
+
+      return res.status(200).json({
+          hash
+      });
+    } catch (err) {
+      return next({
+        status: 400,
+        message: err.message,
+      });
+    }
+  };
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const handle = require('../handlers');
 
 router.get('/generateDummyHash', handle.generateDummyHash);
+router.post('/calculateHash', handle.calculateHash);
 
 module.exports = router;
