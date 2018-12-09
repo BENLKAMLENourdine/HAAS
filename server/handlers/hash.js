b35696a9cee36b49740955be85141bacc6ba17ef
BENLKAMLENourdine <benlkamle.tiznit.95@gmail.com>
2018-12-09 02:16:25 +0100

    finished the backend

--- server/handlers/hash.js
+++ server/handlers/hash.js
@@ -1,4 +1,7 @@
 const Crypto = require('crypto-js');
+const Cryptomd5 = require('crypto-js/md5')
+const CryptoSHA1 = require('crypto-js/sha1')
+const CryptoSHA256 = require('crypto-js/sha256')
 
 exports.generateDummyHash = async (req, res, next) => {
   try {
@@ -15,20 +18,33 @@ exports.generateDummyHash = async (req, res, next) => {
 
 exports.calculateHash = async (req, res, next) => {
     try {
-        const data = req.body.data
+        var data = req.body.data
         const iteration = req.body.iteration
         const algorithm = req.body.algorithm
 
         var hashedData
-        if(algorithm == "md5")
-          hashedData = await calculateHashWithMD5(data, iteration)
-        if(algorithm == "sha1")
-          hashedData = await calculateHashWithSHA1(data, iteration)
-        else(algorithm == "sha256")
-          hashedData = await calculateHashWithSHA256(data, iteration)
+        
+        if(algorithm == "md5"){
+          for(var i=0; i<iteration; i++){
+            hashedData = await Cryptomd5(data)
+            data = hashedData
+          }
+        }
+        else if(algorithm == "sha1"){
+          for(var i=0; i<iteration; i++){
+            hashedData = await CryptoSHA1(data)
+            data = hashedData
+          }
+        }
+        else{
+          for(var i=0; i<iteration; i++){
+            hashedData = await CryptoSHA256(data)
+            data = hashedData
+          }
+        }
   
       return res.status(200).json({
-        hash: hashedData
+        hash: hashedData.toString()
       });
     } catch (err) {
       return next({
