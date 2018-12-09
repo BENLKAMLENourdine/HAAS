73f8e190f4026dc0bf1dea132226713fe7de8714
BENLKAMLENourdine <benlkamle.tiznit.95@gmail.com>
2018-12-09 03:18:39 +0100

    test request from front to back end with axios

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import api from '../services/api'
 
 class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            dummyHash: null
+        }
+    }
+    async componentDidMount(){
+        try {
+            const { hash } = await api.call('get', `generateDummyHash`);
+        this.setState({dummyHash: hash})
+        } catch (error) {
+            
+        }
+    }
     render(){
         return(
-            <div>App works</div>
+            <div>App works {this.state.dummyHash}</div>
         )
     }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const handle = require('../handlers');
 const auth = require('../middleware/');
 
-router.get('/generateDummyHash', auth, handle.generateDummyHash);
+router.get('/generateDummyHash', handle.generateDummyHash);
 router.post('/calculateHash', auth, handle.calculateHash);
 router.post('/login', handle.login);
 router.post('/register', handle.register);
